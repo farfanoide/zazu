f5502e8c30fbaac33ec9613db8eb6fb83f103631
linonetwo <linonetwo012@gmail.com>
2020-04-04 01:30:40 +0800

    test: print console.log from spectron in failed feature

--- features/support/after_hooks.js
+++ features/support/after_hooks.js
@@ -1,5 +1,5 @@
 /* eslint-disable unicorn/filename-case */
-import { After, Before } from 'cucumber'
+import { After, Before, Status } from 'cucumber'
 import jetpack from 'fs-jetpack'
 import path from 'path'
 import git from '../../app/lib/git'
@@ -32,7 +32,25 @@ Before(function () {
 })
 
 After(function (testCase) {
-  return this.close()
+  return Promise.resolve()
+    .then(async () => {
+      if (testCase.result.status === Status.FAILED) {
+        console.log('main:\n---\n')
+        await this.app.client.getMainProcessLogs().then(function (logs) {
+          logs.forEach(function (log) {
+            console.log(log, '\n')
+          })
+        })
+        console.log('renderer:\n---\n')
+        await this.app.client.getRenderProcessLogs().then(function (logs) {
+          logs.forEach(function (log) {
+            console.log(log.message, '\n')
+          })
+        })
+        console.log('\n')
+      }
+      return this.close()
+    })
     .then(() => {
       return git.git(['checkout', databaseFile])
     })
